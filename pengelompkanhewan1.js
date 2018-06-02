639a8023bcda31dc2e357cebb5d2fc264bec862d
albertluck88 <39264172+albertluck88@users.noreply.github.com>
2018-06-02 20:12:15 +0700

    selesai pengelompokan hewan

---
+++ pengelompkanhewan1.js
@@ -0,0 +1,36 @@
+function groupAnimals(animals) {
+  // CARI HURUF PERTAMA 
+  let hurufPertama = []
+  let duplikat = []
+  for(let i = 0; i < animals.length; i++){
+      if(animals.indexOf(animals[i][0]) === -1){
+      hurufPertama.push(animals[i][0])
+      }
+  } 
+hurufPertama.sort(function (a, b){return a > b})
+  for (let o = 0 ; o < hurufPertama.length; o++){
+    if(hurufPertama[o] !== hurufPertama[o+1]){
+      duplikat.push(hurufPertama[o])
+    }
+  }
+  // BKIN GROUP 
+  let groups = []
+  for(let j = 0; j < duplikat.length; j++){
+  let group = []
+  for(let k = 0; k < animals.length; k++){
+    if(animals[k][0] === duplikat[j]){
+      group.push(animals[k])
+    }
+  }
+  groups.push(group) 
+}
+return groups
+
+}
+
+
+// TEST CASES
+console.log(groupAnimals(['cacing', 'ayam', 'kuda', 'anoa', 'kancil']));
+// [ ['ayam', 'anoa'], ['cacing'], ['kuda', 'kancil'] ]
+console.log(groupAnimals(['cacing', 'ayam', 'kuda', 'anoa', 'kancil', 'unta', 'cicak' ]));
+// [ ['ayam', 'anoa'], ['cacing', 'cicak'], ['kuda'], ['unta'] ]
